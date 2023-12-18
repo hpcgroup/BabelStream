72962f3d2ee1ffbbfb8a4dd68fcc1c850c392df1
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-12-18 16:57:05 -0500

    style: 4 space indentation to 2 space indentation

--- src/raja/RAJAStream.cpp
+++ src/raja/RAJAStream.cpp
@@ -44,26 +44,26 @@ void RAJAStream<T>::read_arrays(
         std::vector<T>& a, std::vector<T>& b, std::vector<T>& c)
 {
 #if defined(RAJA_ENABLE_CPU) || defined(BABELSTREAM_MANAGED_ALLOC)
-    std::copy(d_a, d_a + array_size, a.data())
-    std::copy(d_b, d_b + array_size, b.data())
-    std::copy(d_c, d_c + array_size, c.data())
+  std::copy(d_a, d_a + array_size, a.data())
+  std::copy(d_b, d_b + array_size, b.data())
+  std::copy(d_c, d_c + array_size, c.data())
 #else
-    umpire::Allocator host_alloc = rm.getAllocator("HOST");
-    T *h_a = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
-    T *h_b = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
-    T *h_c = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
+  umpire::Allocator host_alloc = rm.getAllocator("HOST");
+  T *h_a = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
+  T *h_b = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
+  T *h_c = static_cast<T*>(host_alloc.allocate(sizeof(T) * array_size));
 
-    rm.copy(h_a, d_a);
-    rm.copy(h_b, d_b);
-    rm.copy(h_c, d_c);
+  rm.copy(h_a, d_a);
+  rm.copy(h_b, d_b);
+  rm.copy(h_c, d_c);
 
-    std::copy(h_a, h_a + array_size, a.data());
-    std::copy(h_b, h_b + array_size, b.data());
-    std::copy(h_c, h_c + array_size, c.data());
+  std::copy(h_a, h_a + array_size, a.data());
+  std::copy(h_b, h_b + array_size, b.data());
+  std::copy(h_c, h_c + array_size, c.data());
 
-    host_alloc.deallocate(h_a);
-    host_alloc.deallocate(h_b);
-    host_alloc.deallocate(h_c);
+  host_alloc.deallocate(h_a);
+  host_alloc.deallocate(h_b);
+  host_alloc.deallocate(h_c);
 #endif
 }
 
